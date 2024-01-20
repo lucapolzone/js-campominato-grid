920e4387f2627869bf0ce2eeda8fdc8d90566fff
lucapolzone <lucapolzone@gmail.com>
2024-01-20 10:46:03 +0100

    feat: rethink exercise with functions

--- js/main.js
+++ js/main.js
@@ -1,49 +1,50 @@
+//### Funzioni
+
+// funzione "genera-cella"
+function createBox(text) {
+  //Crea elemento div
+  let box = document.createElement("div");
+
+  //Assegna la classe "box"
+  box.classList.add("box");
+
+  //Aggiungi il numero, prima usavo uno span, ora è il numero direttamente
+  box.innerText = text;
+
+  //Toggle
+  box.addEventListener("click", function() {
+    box.classList.toggle("clicked");
+    console.log(box.innerText);
+  });
+
+  return box;
+}
+
+
+
+// funzione "genera-griglia"
+function createGrid(container) {
+  container.innerHTML = ''; //questa mi fa svuotare il container perché altrimenti le griglie da 100 si aggiungono, una sotto l'altra
+
+  for (let i = 1; i <= 100; i++) {
+    
+    let grid = createBox(i); //ha richiamato la funzione sopra che fa tutte le sue operazioni e poi ritorna box. Quindi box ora viene inserita in grid 100 volte
+
+    //ora la costante grid deve essere inserita nel container
+    container.append(grid);
+  }
+}
+
+
+//### Richiamo le funzioni
+
+
 // richiamo l'elemento html container della griglia
 const gridContainer = document.getElementById("container");
-//* prima funzione: genera griglia
-// function generateGrid(container){
-  // console.log(container); // ...dovrei visualizzare questo: <div id="container"></div>
-  // .....
-  // .....
-  // .....
-//};
-//generateGrid(gridContainer);
-
-//*seconda funzione: genera cella
-
-
-
-
 const button = document.getElementById("button");
 
 
-//aggiungo la funzione all'evento sul bottone
+// Aggiungo la funzione all'evento sul bottone
 button.addEventListener("click", function() {
-  for(i = 0; i < 100; i++) {
-    //crea elemento div
-    let box = document.createElement("div"); 
-
-    //assegna classe box quindi diventa: <div class="box"></div>
-    box.classList.add("box");
-    
-    //qua gli va a inserire il numero con uno span. + 1 perché le celle mi partono da 0 
-    box.innerHTML += `<span>${i+1}</span>`;
-
-    //toggle
-    box.addEventListener("click", function(){
-      box.classList.toggle("clicked");
-      // this.classList.toggle("clicked");
-      
-      console.log(box.querySelector("span").innerText);
-      // console.log(this.querySelector("span").innerText);
-
-    });
-    
-
-    //
-    gridContainer.append(box);
-  }
-
+  createGrid(gridContainer); //la funzione createGrid(container) è stata popolata con la const gridContainer
 });
-
-
